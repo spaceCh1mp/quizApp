69ae3bced282cbd627045d6070ce8afd2642c140
drimboat <kenechukwuagugua@gmail.com>
2019-04-22 23:52:34 +0100

    parse csv, start quiz

--- quiz.go
+++ quiz.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
+type quiz struct {
+	question string
+	answer   string
+}
+
+func main() {
+	csvFile := flag.String("csv", "problems.csv", "A csv file in the format (question, answer)")
+	flag.Parse()
+
+	file, err := os.Open(*csvFile)
+	if err != nil {
+		exit(fmt.Sprintf("Could not open file: %s", *csvFile))
+	}
+	r := csv.NewReader(file)
+	records, err := r.ReadAll()
+	if err != nil {
+		exit(fmt.Sprintf("Could not read file: %s", *csvFile))
+	}
+	problem := parseRecords(records)
+	score := startQuiz(problem)
+	fmt.Printf("Your score is %d out of %d", score, len(problem))
+}
+
+func startQuiz(problems []quiz) int {
+	var s int = 0
+	var a string
+	for i, p := range problems {
+		fmt.Printf("Q%d: %s = \n", i+1, p.question)
+		fmt.Scanf("%s\n", &a)
+		if a == p.answer {
+			s += 1
+		}
+	}
+	return s
+}
+func parseRecords(records [][]string) []quiz {
+	ret := make([]quiz, len(records))
+	for i, record := range records {
+		ret[i] = quiz{
+			question: record[0],
+			answer:   record[1],
+		}
+	}
+	return ret
+}
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
