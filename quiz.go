d7a7264d66061b189ac9371e8aa0eb1f4bdf597a
drimboat <kenechukwuagugua@gmail.com>
2019-04-25 02:53:40 +0100

    Added shuffle option, fixed issue of having to wait for time to expire after completing quiz

--- quiz.go
+++ quiz.go
@@ -4,9 +4,13 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
+
+var sum int = 0
 
 type quiz struct {
 	question string
@@ -16,6 +20,7 @@ type quiz struct {
 func main() {
 	csvFile := flag.String("csv", "problems.csv", "A csv file in the format (question, answer)")
 	limit := flag.Int("limit", 30, "The timer is in seconds")
+	shuffle := flag.Bool("shuffle", false, "Quiz doesn't shuffle by default, pass \"true\" as a value to shuffle")
 	flag.Parse()
 
 	file, err := os.Open(*csvFile)
@@ -31,25 +36,41 @@ func main() {
 	timer := time.NewTimer(time.Duration(*limit) * time.Second)
 	fmt.Printf("Total number of questions to answer: %d \n", len(problem))
 	fmt.Println("Time alloted: ", *limit)
-	score := startQuiz(problem, timer)
-	fmt.Printf("Your score is %d out of %d", score, len(problem))
+	if *shuffle {
+		rand.Seed(time.Now().UTC().UnixNano())
+		rand.Shuffle(len(problem), func(i, j int) {
+			problem[i], problem[j] = problem[j], problem[i]
+		})
+	}
+	startQuiz(problem, timer)
+	fmt.Printf("Your score is %d out of %d", sum, len(problem))
 }
 
-func startQuiz(problems []quiz, limit *time.Timer) int {
-	var s int = 0
-	var a string
+func startQuiz(problems []quiz, limit *time.Timer) {
+	c := make(chan bool)
 	go func() {
+		var a string
 		for i, p := range problems {
 			fmt.Printf("Q%d: %s = \n", i+1, p.question)
 			fmt.Scanf("%s\n", &a)
-			if a == p.answer {
-				s += 1
+			a = cleanup(a)
+			ans := cleanup(p.answer)
+			if a == ans {
+				sum += 1
 			}
 		}
+		c <- true
 	}()
-	<-limit.C
-	fmt.Printf("\nYour time is up\n")
-	return s
+	var m string
+	select {
+	case <-limit.C:
+		m = "\nYour time is up"
+		break
+	case <-c:
+		m = "\nYou Finished!"
+		break
+	}
+	fmt.Println(m)
 }
 func parseRecords(records [][]string) []quiz {
 	ret := make([]quiz, len(records))
@@ -60,6 +81,9 @@ func parseRecords(records [][]string) []quiz {
 		}
 	}
 	return ret
+}
+func cleanup(str string) string {
+	return strings.TrimSpace(strings.ToLower(str))
 }
 func exit(msg string) {
 	fmt.Println(msg)
